9a5c43fd70e534193433fddc22783dd858c456b4
theSirD <isaev-daniel17@yandex.ru>
2023-08-11 22:54:03 +0300

    Creates layout for CV preview

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Generator from './generator/Generator.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Generator from "./generator/Generator.jsx";
+import ResultCV from "./resultCV/ResultCV";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Generator />
-  </React.StrictMode>,
-)
+    <ResultCV />
+  </React.StrictMode>
+);
