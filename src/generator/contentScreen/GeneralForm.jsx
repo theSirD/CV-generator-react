d6842e731d82a2313d05f081b962efbb6862100e
theSirD <isaev-daniel17@yandex.ru>
2023-08-15 22:10:10 +0300

    Improve layoud for generator (left menu)

--- src/generator/contentScreen/GeneralForm.jsx
+++ src/generator/contentScreen/GeneralForm.jsx
@@ -1,15 +1,21 @@
 export default function GeneralForm() {
   return (
-    <div>
-      <div className="form-header">General info</div>
-      <form action="" method="get" className="form-general">
-        <label htmlFor="name">Name </label>
-        <input type="text" name="name" id="name" required />
-        <label htmlFor="email">Email </label>
-        <input type="email" name="email" id="email" required />
-        <label htmlFor="tel">Tel </label>
-        <input type="tel" name="tel" id="tel" required />
-        <div className="general-form-buttons">
+    <div className="general-info-container">
+      <div className="general-info-header">General info</div>
+      <form action="" method="" className="form-general">
+        <div className="field">
+          <label htmlFor="name">Name </label>
+          <input type="text" name="name" id="name" required />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Email </label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="field">
+          <label htmlFor="tel">Tel </label>
+          <input type="tel" name="tel" id="tel" required />
+        </div>
+        <div className="buttons-row">
           <input
             type="file"
             id="avatar"
